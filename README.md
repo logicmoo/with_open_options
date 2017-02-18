33fb5aaf770489d0740fd094912983fa4420c291
root <logicmoo@gmail.com>
2017-02-18 11:06:34 -0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # open_anything
 Utilities to open various objects for read/write
+
+```prolog
+
+:- pack_install('https://github.com/TeamSPoon/with_open_options.git').
+
+```
+
+
+```prolog
+
+:- use_module(library(filestreams)).
+
+% makes a simple API for reading things
+
+```
+
+
+```prolog
+
+:- use_module(library(filesystem)).
+
+% mass wildcard expansions
+
+```
+
+
+TODO Document this pack!
+
---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(with_open_options).
+version('0.0.1').
+title('Utilities to open various objects for read/write').
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+home( 'https://github.com/TeamSPoon/with_open_options' ).
+download( 'https://github.com/TeamSPoon/with_open_options/release/*.zip' ).
+provides(filestreams).
