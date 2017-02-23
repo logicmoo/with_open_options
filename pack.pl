9468132435af4557e0bd01f382e5f8f1ee7d6623
root <logicmoo@gmail.com>
2017-02-23 09:25:00 -0800

    v0.0.8

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(with_open_options).
-version('0.0.7').
+version('0.0.8').
 title('Utilities to open various objects for read/write').
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
