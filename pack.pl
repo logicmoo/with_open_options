ea40c3c7d29cb8dcf67fdfc5568012bfe5305044
root <logicmoo@gmail.com>
2017-02-23 07:54:14 -0800

    v0.0.7

--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(with_open_options).
-version('0.0.1').
+version('0.0.7').
 title('Utilities to open various objects for read/write').
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
-home( 'https://github.com/TeamSPoon/pack_with_open_options' ).
-download( 'https://github.com/TeamSPoon/pack_with_open_options/release/*.zip' ).
+home( 'https://github.com/TeamSPoon/with_open_options' ).
+download( 'https://github.com/TeamSPoon/with_open_options/release/*.zip' ).
 provides(filestreams).
 autoload(true).
